1b1063e44fe7491ce0fafde38a8f98acb55f12d7
Shawn Meyer <shawn@shawnmeyer.com>
2013-10-11 19:47:24 -0500

    Minify and Copy JS dependencies and code
    This will copy the src directory contents to the build/src dir.  The
    libs are copied, but the custom *.js files will be minified/uglified.

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -1,6 +1,27 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+		  options: {
+		    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		  },
+		  build: {
+		    src: ['src/**/*.js', '!src/libs/**'],
+		    dest: 'build/src/<%= pkg.name %>.min.js'
+		  }
+		},
+		copy: {
+    	  dev: {
+            files: [{
+              src: 'src/libs/*.js',
+              dest: 'build/'
+            }]
+          }
+        }
 	});
 
-	grunt.registerTask('default', []);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', ['uglify', 'copy']);
 }
