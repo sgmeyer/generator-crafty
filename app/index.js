9aab6d00c28ab3c732e750ea553ea8c62a6f225f
Shawn Meyer <shawn@shawnmeyer.com>
2013-10-13 07:47:38 -0500

    Updating files with appname
    - Removed file list
    - Default game name is 'appname' <- parent directory
    - cleanup of index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 
-
 var CraftyGenerator = module.exports = function CraftyGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
@@ -19,56 +18,68 @@ util.inherits(CraftyGenerator, yeoman.generators.Base);
 CraftyGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
-  // have Yeoman greet the user.
   console.log(this.yeoman);
 
   var prompts = [{
     name: 'gameName',
     message: 'What is the name of your game?',
-    default: ('game')
+    default: (this.appname)
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.gameName;
+    this.gameName = props.gameName;
 
     cb();
   }.bind(this));
 };
 
 CraftyGenerator.prototype.app = function app() {
-  this.copy('_package.json', 'package.json');
-  this.copy('_Gruntfile.js', 'Gruntfile.js');
-  this.copy('_bower.json', 'bower.json');
-  this.copy('_index.html', 'index.html');
 };
 
 CraftyGenerator.prototype.projectfiles = function projectfiles() {
-  this.mkdir('src');
-    this.mkdir('src/components');
-    this.mkdir('src/entities');
-      this.mkdir('src/entities/base');
-    this.mkdir('src/interfaces');
-    this.mkdir('src/levels');
-    this.mkdir('src/libs');
-    this.mkdir('src/scenes');
-    this.mkdir('windows');
-  this.mkdir('web');
-  this.mkdir('web/images');
+  this.mkdir('app')
+  this.mkdir('app/src');
+    this.mkdir('app/src/components');
+    this.mkdir('app/src/entities');
+      this.mkdir('app/src/entities/base');
+    this.mkdir('app/src/interfaces');
+    this.mkdir('app/src/levels');
+    this.mkdir('app/src/libs');
+    this.mkdir('app/src/scenes');
+    this.mkdir('app/windows');
+  this.mkdir('app/web');
+  this.mkdir('app/web/images');
   
   this.copy('editorconfig', '.editorconfig');
   this.copy('jshintrc', '.jshintrc');
 
-  this.copy('src/_config.js', 'src/config.js');
-  this.copy('src/_game.js', 'src/game.js');
-  this.copy('src/_sprites.js', 'src/sprites.js');
-  this.copy('src/components/_MouseHover.js', 'src/components/MouseHover.js');
-  this.copy('src/entities/base/_BaseEntity.js', 'src/entities/base/BaseEntity.js');
-  this.copy('src/interfaces/_info.js', 'src/interfaces/info.js');
-  this.copy('src/libs/_backbone-0.9.2.min.js', 'src/libs/backbone-0.9.2.min.js');
-  this.copy('src/libs/_crafty-0.4.9.js', 'src/libs/crafty-0.4.9.js');
-  this.copy('src/libs/_jquery-1.7.2.min.js', 'src/libs/jquery-1.7.2.min.js');
-  this.copy('src/libs/_modernizr-2.5.3.min.js', 'src/libs/modernizr-2.5.3.min.js');
-  this.copy('src/libs/_require-jquery.js', 'src/libs/require-jquery.js');
-  this.copy('src/libs/_underscore-1.3.4.min.js', 'src/libs/underscore-1.3.4.min.js');
-  this.copy('src/scenes/_main.js', 'src/scenes/main.js');
+  this.copy('src/_config.js', 'app/src/config.js');
+  this.copy('src/_game.js', 'app/src/game.js');
+  this.copy('src/_sprites.js', 'app/src/sprites.js');
+  this.copy('src/components/_MouseHover.js', 'sapp/rc/components/MouseHover.js');
+  this.copy('src/entities/base/_BaseEntity.js', 'app/src/entities/base/BaseEntity.js');
+  this.copy('src/interfaces/_info.js', 'app/src/interfaces/info.js');
+  this.copy('src/libs/_backbone-0.9.2.min.js', 'app/src/libs/backbone-0.9.2.min.js');
+  this.copy('src/libs/_crafty-0.4.9.js', 'app/src/libs/crafty-0.4.9.js');
+  this.copy('src/libs/_jquery-1.7.2.min.js', 'app/src/libs/jquery-1.7.2.min.js');
+  this.copy('src/libs/_modernizr-2.5.3.min.js', 'app/src/libs/modernizr-2.5.3.min.js');
+  this.copy('src/libs/_require-jquery.js', 'app/src/libs/require-jquery.js');
+  this.copy('src/libs/_underscore-1.3.4.min.js', 'app/src/libs/underscore-1.3.4.min.js');
+  this.copy('src/scenes/_main.js', 'app/src/scenes/main.js');
 };
+
+CraftyGenerator.prototype.gruntfileJSON = function gruntfile() {
+  this.copy('_Gruntfile.js', 'Gruntfile.js');
+};
+
+CraftyGenerator.prototype.packageJSON = function packageJSON() {
+  this.copy('_package.json', 'package.json');
+};
+
+CraftyGenerator.prototype.bowerJSON = function bower() {
+  this.copy('_bower.json', 'bower.json');
+};
+
+CraftyGenerator.prototype.indexHTML = function writeIndex() {
+  this.copy("_index.html", "index.html");
+}
