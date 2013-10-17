f359671c211900d83227dcbf9bb95515e239b55d
Shawn Meyer <sgmeyer@gmail.com>
2013-10-17 11:47:53 -0500

    Adding better build for copy and minify

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -6,21 +6,76 @@ var mountFolder = function (connect, dir) {
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		clean: ['build/'],
+		clean: {
+            release:['build/dev/'],
+            dev:['build/release/']
+        },
 		uglify: {
-		  options: {
-		    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-		  },
-		  build: {
-		    src: ['src/**/*.js', '!src/libs/**'],
-		    dest: 'build/src/<%= pkg.name %>.min.js'
-		  }
+    		options: {
+    	       banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    		},
+    		build: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    cwd: 'app/src/libs/',
+        		    src: 'jquery/jquery.js',
+        		    dest: 'build/dev/src/libs/',
+                    ext: '.min.js'
+        		  },
+                  {
+                    expand: true,
+                    flatten: true,
+                    cwd: 'app/src/libs/',
+                    src: 'modernizr/modernizr.js',
+                    dest: 'build/dev/src/libs/',
+                    ext: '.min.js'
+                  },
+                  {
+                    'build/dev/src/libs/requirejs-jquery.min.js': ['app/src/libs/requirejs-jquery/parts/require.js', 'app/src/libs/requirejs-jquery/parts/query.js']
+                  }]
+            }
 		},
 		copy: {
     	  dev: {
             files: [{
-              src: 'src/libs/*.js',
-              dest: 'build/'
+              expand: true,
+              flatten: true,
+              cwd: 'app/src/libs/',
+              src: 'backbone/backbone-min.js',
+              dest: 'build/dev/src/libs/',
+              filter: 'isFile'
+            },
+            {
+              expand: true,
+              flatten: true,
+              cwd: 'app/src/libs/',
+              src: 'crafty/crafty.min.js',
+              dest: 'build/dev/src/libs/',
+              filter: 'isFile'
+            },
+            {
+                expand: true,
+                flatten: true,
+                cwd: 'app/src/libs/',
+                src: 'underscore/underscore-min.js',
+                dest: 'build/dev/src/libs/',
+                filter: 'isFile'
+            },
+            {
+                expand: true,
+                cwd: 'app/src/',
+                src: ['**/*.js', '!libs/**'],
+                dest: 'build/dev/src/',
+                filter: 'isFile'
+            },
+            {
+                expand: true,
+                flatten: true,
+                cwd: 'app/',
+                src: 'index.html',
+                dest: 'build/dev/',
+                filter: 'isFile'
             }]
           }
         },
@@ -29,7 +84,7 @@ module.exports = function(grunt) {
 		      options: {
 		        port: 8888,
 		        hostname: 'localhost',
-		        base: "app"
+		        base: "build/dev"
 		      }
 		    },
 	        livereload: {
