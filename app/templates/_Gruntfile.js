2646646396ecc659fd13d6f82c364d0459516d16
Vesa H <weeezes@gmail.com>
2014-03-22 11:33:08 +0200

    cleaned gruntfile's whitespaces

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -4,33 +4,33 @@ var mountFolder = function (connect, dir) {
 };
 
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		clean: ['dist'],
-		uglify: {
-			build: {
-				files: [{
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['dist'],
+    uglify: {
+      build: {
+        files: [{
                     expand: true,
                     cwd: 'app/src/',
                     src: ['**/*.js', '!libs/**/*', '!bower_components/**/*'],
                     dest: 'dist/src/',
                     ext: '.js'
-				},
-				{
+        },
+        {
                     expand: true,
                     flatten: true,
                     cwd: 'app/src/brower_components/',
-        		    src: ['jquery/jquery.js', 'modernizr/modernizr.js'],
-        		    dest: 'dist/src/libs/',
+                src: ['jquery/jquery.js', 'modernizr/modernizr.js'],
+                dest: 'dist/src/libs/',
                     ext: '.min.js'
-        		},
+            },
                 {
                     'dist/src/libs/requirejs-jquery.min.js': ['app/src/bower_components/requirejs-jquery/parts/require.js', 'app/src/bower_components/requirejs-jquery/parts/query.js']
                 }]
-			}
-		},
-		copy: {
-    	  dist: {
+      }
+    },
+    copy: {
+        dist: {
             files: [{
               expand: true,
               flatten: true,
@@ -46,60 +46,60 @@ module.exports = function(grunt) {
               src: ['**/*'],
               dest: 'dist/src/libs/',
               filter: 'isFile'
-            }, 
+            },
             {
-            	expand: true,
-            	cwd: 'app/web',
-            	src: ['**/*'],
-            	dest: 'dist/web',
-            	filter: 'isFile'
+              expand: true,
+              cwd: 'app/web',
+              src: ['**/*'],
+              dest: 'dist/web',
+              filter: 'isFile'
             }]
           }
         },
         connect: {
-        	server: {
-		      options: {
-		        port: 8888,
-		        hostname: 'localhost',
-		        base: "app/"
-		      }
-		    },
-	        livereload: {
-	          options: {
-	            middleware: function (connect) {
-	              return [require('connect-livereload')({port: LIVERELOAD_PORT}),
-	                mountFolder(connect, '.')
-	              ];
-	            }
-	          }
+          server: {
+          options: {
+            port: 8888,
+            hostname: 'localhost',
+            base: "app/"
+          }
+        },
+          livereload: {
+            options: {
+              middleware: function (connect) {
+                return [require('connect-livereload')({port: LIVERELOAD_PORT}),
+                  mountFolder(connect, '.')
+                ];
+              }
+            }
             }
         },
         open: {
-	      server: {
-	        path: 'http://localhost:<%= connect.server.options.port %>'
-	      }
-	    },
+        server: {
+          path: 'http://localhost:<%= connect.server.options.port %>'
+        }
+      },
         watch: {
             html: {
                 options: { livereload: true },
                 files: ['app/index.html'],
             },
-	        js: {
+          js: {
                 options: { livereload: true },
                 files: ['app/src/**/*.js']
             }
-	    }
-	});
+      }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-livereload');
-	grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-open');
 
-	grunt.registerTask('build', ['clean', 'uglify', 'copy']);
-	grunt.registerTask('server', ['connect:server', 'open', 'watch']);
-	grunt.registerTask('default', ['server']);
+  grunt.registerTask('build', ['clean', 'uglify', 'copy']);
+  grunt.registerTask('server', ['connect:server', 'open', 'watch']);
+  grunt.registerTask('default', ['server']);
 }
