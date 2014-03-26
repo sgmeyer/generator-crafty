e11ff87745dc0c6518e19fe9e8b360a2ee120330
Shawn Meyer <sgmeyer@gmail.com>
2014-03-26 10:58:55 -0500

    Cleaning up merge

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -6,95 +6,6 @@ var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
 
-<<<<<<< HEAD
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		clean: ['dist'],
-		uglify: {
-			build: {
-				files: [{
-          expand: true,
-          cwd: 'app/src/',
-          src: ['**/*.js', '!libs/**/*', '!bower_components/**/*'],
-          dest: 'dist/src/',
-          ext: '.js'
-				},
-				{
-          expand: true,
-          flatten: true,
-          cwd: 'app/src/brower_components/',
-  		    src: ['jquery/jquery.js', 'modernizr/modernizr.js'],
-  		    dest: 'dist/src/libs/',
-          ext: '.min.js'
-    		},
-        {
-          'dist/src/libs/requirejs-jquery.min.js': ['app/src/bower_components/requirejs-jquery/parts/require.js', 'app/src/bower_components/requirejs-jquery/parts/query.js']
-        }]
-			}
-		},
-		copy: {
-    	  dist: {
-          files: [{
-            expand: true,
-            flatten: true,
-            cwd: 'app/src/bower_components/',
-            src: ['backbone/backbone-min.js', 'backbone/backbone-min.map', 'underscore/underscore-min.js', 'underscore/underscore-min.map'],
-            dest: 'dist/src/libs/',
-            filter: 'isFile'
-          },
-          {
-            expand: true,
-            flatten: true,
-            cwd: 'app/src/libs/',
-            src: ['**/*'],
-            dest: 'dist/src/libs/',
-            filter: 'isFile'
-          }, 
-          {
-          	expand: true,
-          	cwd: 'app/web',
-          	src: ['**/*'],
-          	dest: 'dist/web',
-          	filter: 'isFile'
-          }]
-        }
-      },
-      connect: {
-      	server: {
-  	      options: {
-  	        port: 8888,
-  	        hostname: 'localhost',
-  	        base: "app/"
-  	      }
-        },
-        livereload: {
-          options: {
-            middleware: function (connect) {
-              return [require('connect-livereload')({port: LIVERELOAD_PORT}),
-                mountFolder(connect, '.')
-              ];
-            }
-          }
-        }
-      },
-      open: {
-	      server: {
-	        path: 'http://localhost:<%= connect.server.options.port %>'
-	      }
-	    },
-      watch: {
-        html: {
-          options: { livereload: true },
-          files: ['app/index.html'],
-        },
-        js: {
-          options: { livereload: true },
-          files: ['app/src/**/*.js']
-        }
-	    }
-	});
-=======
 // # Globbing
 // for performance reasons we're only matching one level down:
 // 'test/spec/{,*/}*.js'
@@ -349,7 +260,6 @@ module.exports = function (grunt) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
     }
->>>>>>> bd10ddbf8732c0e714aef30dafae4eec3374c87a
 
     grunt.task.run([
       'clean:server',
